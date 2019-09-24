758d76a14062eddaeeb81312c3194abc10bb8cbc
unknown <carbagc30@gmail.com>
2019-09-24 10:30:00 -0500

    mi primer intento de proyecto

---
+++ 01INTRODUCCION/js/index.js
@@ -0,0 +1,27 @@
+// Una variable se llama nombre y le estou asignando el valor de erick y es una variable con letras
+var nombre = "erick ruben camacho";
+// Esta es una variable de numeros y es una variable de edad
+var edad = 17;
+// Una variable de peso
+var peso = 57;
+var saludo = "hola mundo;"
+console.log(saludo);
+var altura = 174;
+var medida = 95;
+var apeido = "camacho";
+console.log(apeido)
+console.log(medida)
+console.log(altura)
+console.log(altura + medida + apeido);
+// error sumo las dos ultimas variables de tipo númerico
+console.log(altura , medida + apeido);
+// respeta la diferencia de variables
+console.log(altura , medida , apeido);
+// para que no sume como una calculadora los numeros, en vez de poner signo de mas, se le pone una coma para que no sume y saque un resultado sino que lo deje como esta
+console.log(altura + "\t" , medida + "\n" , peso);
+console.log("**********");
+console.log("NOMBRE : " + nombre );
+console.log("EDAD : " + edad);
+console.log("PESO : "+ peso);
+console.log("**********");
+console.log("********** \n NOMBRE: "+ nombre +"\n EDAD : " + edad + "n\ PESO: " + peso + "\n **********");
---
+++ Operaciones/Js/operation.js
@@ -0,0 +1,25 @@
+// prompt es palabra reservada del lenguaje propio;
+// PROMPT interactuar con la pagina;
+// prompt("Dime tu nombre", "ejemplo de nombre");
+console.log("HOLA MUNDO");
+var nombre = "erick";
+var edad = 17;
+var altura = 174;
+var nombre = prompt
+console.log(nombre);
+console.log(edad);
+console.log(altura);
+// sintaxis para funciones y eventos;
+// palabra reservada function + nombre del evento + parentesis + llaves;
+function Ingresar() {
+    console.log("SALUDAR");
+    
+}
+console.log("tu nombre es ----> "+ apeido);
+
+
+
+
+// prompt("Erick", "ejemplo de nombre");
+
+
